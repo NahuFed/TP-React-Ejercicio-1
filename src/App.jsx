8c84ffdd07ccfaec22cfb6560864164b489825a3
Nahuel Rodriguez <nahufed@gmail.com>
2023-06-01 04:52:47 -0300

    agregado footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Container } from 'react-bootstrap';
 import './App.css'
 import HolaMundo from './components/HolaMundo.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() { 
 
   return (
     <>
-    <HolaMundo></HolaMundo>      
+    <Container className='main p-5'>
+    <HolaMundo></HolaMundo> 
+    </Container>
+    <footer className="bg-dark text-center text-light py-4">
+        <p>&copy; Todos los derechos reservados </p>
+      </footer>     
+
     </>
   )
 }
